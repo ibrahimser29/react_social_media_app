f20770ad5a75a6a4934420ce37bf202a3cab901d
ibrahimser29 <92850843+ibrahimser29@users.noreply.github.com>
2023-12-10 20:37:54 +0200

    first commit

---
+++ src/lib/utils.ts
@@ -0,0 +1,40 @@
+import { type ClassValue, clsx } from "clsx"
+import { twMerge } from "tailwind-merge"
+ 
+export function cn(...inputs: ClassValue[]) {
+  return twMerge(clsx(inputs))
+}
+
+export function timeAgo(dateString: string): string {
+  const currentDate = new Date();
+  const inputDate = new Date(dateString);
+  const timeDifference = currentDate.getTime() - inputDate.getTime();
+
+  // Calculate time units
+  const seconds = Math.floor(timeDifference / 1000);
+  const minutes = Math.floor(seconds / 60);
+  const hours = Math.floor(minutes / 60);
+  const days = Math.floor(hours / 24);
+
+  if (days > 1) {
+    return `${days} days ago`;
+  } else if (days === 1) {
+    return '1 day ago';
+  } else if (hours > 1) {
+    return `${hours} hours ago`;
+  } else if (hours === 1) {
+    return '1 hour ago';
+  } else if (minutes > 1) {
+    return `${minutes} minutes ago`;
+  } else if (minutes === 1) {
+    return '1 minute ago';
+  } else {
+    return 'just now';
+  }
+}
+
+export const checkIsLiked = (likeList: string[], userId: string) => {
+  return likeList.includes(userId);
+};
+
+export const convertFileToUrl = (file: File) => URL.createObjectURL(file);
---
+++ src/lib/validation/index.ts
@@ -0,0 +1,29 @@
+import * as z from "zod";
+
+export const signupValidation = z.object({
+    name: z.string().min(2,{message:'Too Short'}),
+    username: z.string().min(2,{message:'username must be at least 2 characters'}),
+    email: z.string().email(),
+    password: z.string().min(8,{message:'password must be at least 8 characters'}),
+  })
+
+export const signinValidation = z.object({
+    email: z.string().email(),
+    password: z.string().min(8,{message:'password must be at least 8 characters'}),
+  })
+
+  export const PostValidation = z.object({
+    caption: z.string().min(5).max(2200),
+    file: z.custom<File[]>(),
+    location: z.string().min(2).max(100),
+    tags: z.string()
+  })
+
+  export const ProfileValidation = z.object({
+    file: z.custom<File[]>(),
+    name: z.string().min(2, { message: "Name must be at least 2 characters." }),
+    username: z.string().min(2, { message: "Name must be at least 2 characters." }),
+    email: z.string().email(),
+    bio: z.string(),
+  });
+  
